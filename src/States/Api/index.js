d774a6071bd0def0aeeb09bebd63f3f56e1308cc
nducrypto <agbonud9@gmail.com>
2022-10-31 14:25:27 +0100

    api two

--- src/States/Api/index.js
+++ src/States/Api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://logistic-api.onrender.com/api" });
+const API = axios.create("https://logistic-api.onrender.com/api");
 // const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
 // API.interceptors.request.use((req) => {
