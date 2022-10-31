cf15ccbecb58e8db78194f73806b997afe24f5c5
nducrypto <agbonud9@gmail.com>
2022-10-31 14:30:55 +0100

    api three

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 // const API = axios.create({ baseURL: "http://localhost:5000/api" });
-const API = axios.create("https://logistic-api.onrender.com/api");
+const API = axios.create({ baseURL: "https://logistic-api.onrender.com/api" });
 
 const useFetch = (url) => {
   const [data, setData] = useState([]);
